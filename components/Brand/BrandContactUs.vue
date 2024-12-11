3d2ab3e998d4e0776cf732c2f57e1ce92d9bfbfe
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-12-11 15:34:44 +0500

    accessories ur updates

--- components/Brand/BrandContactUs.vue
+++ components/Brand/BrandContactUs.vue
@@ -4,7 +4,7 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <div class="z-1 relative -mt-[305px] mb-[64px] bg-customPurple pt-[250px] text-white">
-    <div class="h-full bg-[url('/public/images/answer-decor.png')] bg-cover bg-center">
+    <div class="h-full bg-[url('@/assets/images/answer-decor.png')] bg-cover bg-center">
       <div class="mx-auto w-full max-w-[1140px] py-[120px] text-center md:py-[160px]">
         <h2 class="mb-[18px] text-[36px] font-[700] text-white">Cant Find The Answer?</h2>
         <p class="mb-[70px] text-[14px] text-white">Get in touch with our proffessional team and we will get your answer in less then 24h</p>
